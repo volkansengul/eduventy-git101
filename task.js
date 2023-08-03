288d4c654caa49c12d8b1cc7480d568bc4bcc483
Volkan Şengül <iletisim@volkansengul.com>
2023-08-03 22:11:40 +0300

    bir baska commit

---
+++ task.js
@@ -0,0 +1,33 @@
+const people = [
+  { name: "Kutluhan" },
+  { name: "Çağrı" },
+  { name: "Sena" },
+  { name: "Doğu" },
+  { name: "Berna" },
+  { name: "Aytekin" },
+  { name: "Semih" },
+  { name: "Halenur" },
+  { name: "Ufuk" },
+  { name: "Emine" },
+  { name: "Mümtaz" },
+];
+
+const listElm = document.getElementById("list");
+
+people.forEach((item, i) => {
+  listElm.innerHTML += `<li id="user_${i}">${item.name}</li>`;
+});
+
+function getLuckyPerson() {
+  let luckyNumber = Math.floor(Math.random() * people.length);
+  let person = document.getElementById(`user_${luckyNumber}`);
+
+  for (i = 0; i < people.length; i++) {
+    let _person = document.getElementById(`user_${i}`);
+    _person.classList.remove("lucky");
+    _person.innerText = people[i].name;
+  }
+
+  person.classList.add("lucky");
+  person.innerText = `${people[luckyNumber].name} - Lucky Person `;
+}
